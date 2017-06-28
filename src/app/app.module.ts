005d509c1c7b9b94a76837cdd021e3fbf3078e2c
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-06-28 16:50:48 -0400

    VER3-124 step 6 added router module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
-
+import { RouterModule } from '@angular/router';
 import { HeroesComponent }  from './heroes.component';
 import {HeroService} from './hero.service';
 import {AppComponent} from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      }
+    ])
+  ],
   declarations: [ AppComponent,
                   HeroDetailComponent,
                   HeroesComponent],
