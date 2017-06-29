e92f90372784d8fa770f14e0533f09184452acab
alasdair-macleod <alasdair.macleod@ufl.edu>
2017-06-29 10:25:09 -0400

    VER3-124 step 6 factored out routing into routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './hero-detail.component';
-import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 import { HeroesComponent }  from './heroes.component';
 import {HeroService} from './hero.service';
 import {AppComponent} from './app.component';
@@ -12,31 +12,13 @@ import {DashboardComponent} from './dashboard.component';
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      },
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'detail/:id',
-        component: HeroDetailComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [ AppComponent,
+                  DashboardComponent,
                   HeroDetailComponent,
-                  HeroesComponent,
-                  DashboardComponent],
-  bootstrap:    [ AppComponent ],
-  providers:    [ HeroService ]
+                  HeroesComponent],
+  providers:    [ HeroService ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
